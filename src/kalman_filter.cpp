92aa719e7c79a01abd55c3732e1c714a3c95d399
pepere <alexis.preel@gmail.com>
2017-06-02 20:58:12 -0700

    First Commit

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,5 +1,10 @@
+#include <iostream>
+#include <math.h> 
 #include "kalman_filter.h"
+#include "tools.h"
 
+using namespace std;
+using std::vector;
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
@@ -9,31 +14,102 @@ KalmanFilter::~KalmanFilter() {}
 
 void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
                         MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
-  x_ = x_in;
-  P_ = P_in;
-  F_ = F_in;
-  H_ = H_in;
-  R_ = R_in;
-  Q_ = Q_in;
+    x_ = x_in;
+    P_ = P_in;
+    F_ = F_in;
+    H_ = H_in;
+    R_ = R_in;
+    Q_ = Q_in;
 }
 
 void KalmanFilter::Predict() {
-  /**
-  TODO:
-    * predict the state
-  */
+    // Applying state transition matrix
+    x_ = F_ * x_;
+	MatrixXd Ft = F_.transpose();
+    
+    // Recomputing state covariance matrix
+	P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Kalman Filter equations
-  */
+    // Getting prediction in measurement space
+    VectorXd z_pred = H_ * x_;
+    cout << "z_pred = " << z_pred << endl;
+	
+    // Computing difference between prediction and measurement
+    cout << "z = " << z << endl;
+    VectorXd y = z - z_pred;
+	
+    // Adding state transition covariance and measurement covariance matrices
+    MatrixXd Ht = H_.transpose();
+    MatrixXd PHt = P_ * Ht;
+	MatrixXd S = H_ * PHt + R_;
+    
+    // Computing Kalman gain
+	MatrixXd Si = S.inverse();
+	MatrixXd K = PHt * Si;
+
+	// Updating estimate
+	x_ = x_ + (K * y);
+	
+    // Recomputing state covariance matrix
+    long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Extended Kalman Filter equations
-  */
+    // Getting state parameters
+	float px = x_(0);
+	float py = x_(1);
+	float vx = x_(2);
+	float vy = x_(3);
+
+	// Computing radar range
+    float rho = sqrt(px * px + py * py);
+	
+    // Checking denominator for division
+    cout << "rho = " << rho << endl;
+    if (rho < 0.0001) {
+	    cout << "Error in KalmanFilter::UpdateEKF() - No division by zero." << endl;
+	}
+    
+    else {
+        // Computing radar angle and range rate from Cartesion coordinates
+        float phi = atan2(py, px);
+        float rho_dot = (px * vx + py * vy) / rho;
+        
+        // Instantiating measurement vector in polar coordinates
+        VectorXd z_pred = VectorXd(3);
+        z_pred << rho, phi, rho_dot;
+        cout << "z_pred size = " << z_pred.size() << endl;
+        
+        // Computing difference between prediction and measurement
+        VectorXd y = z - z_pred;
+        
+        // Normalizing angle in [-pi,pi]
+        y(1) = remainder(y(1), 2.0 * M_PI);
+        
+        // Computing measurement jacobian matrix Hjs
+        cout << ".x_ = " << x_ << endl;
+        Tools tools;
+        MatrixXd Hj_ = tools.CalculateJacobian(x_);
+        
+        // Adding state transition covariance and measurement covariance matrices
+        MatrixXd Hjt = Hj_.transpose();
+        MatrixXd PHt = P_ * Hjt;
+        MatrixXd S = Hj_ * PHt + R_;
+        
+        // Computing Kalman gain
+        MatrixXd Si = S.inverse();
+        MatrixXd K = PHt * Si;
+
+        // Updating estimate
+        x_ = x_ + (K * y);
+        
+        // Recomputing state covariance matrix
+        long x_size = x_.size();
+        MatrixXd I = MatrixXd::Identity(x_size, x_size);
+        P_ = (I - K * Hj_) * P_;
+    }
 }
--- src/tools.cpp
+++ src/tools.cpp
@@ -11,15 +11,65 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
+    /**
+    TODO:
     * Calculate the RMSE here.
-  */
+    */
+    // Initializing RMSE
+    VectorXd rmse(4);
+	rmse << 0,0,0,0;
+    
+    // Checking inputs
+	if(estimations.size() != ground_truth.size() || estimations.size() == 0){
+		cout << "Invalid inputs for RMSE" << endl;
+		return rmse;
+	}
+    
+	// Cumulating squared residuals
+	for(unsigned int i=0; i < estimations.size(); ++i){
+        
+		VectorXd r = estimations[i] - ground_truth[i];
+        
+		//coefficient-wise multiplication
+		r = r.array() * r.array();
+		rmse += r;
+	}
+    
+	// Calculating mean
+	rmse = rmse / estimations.size();
+    
+	// Calculating squared root
+	rmse = rmse.array().sqrt();
+    
+	// Return result
+	return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-  TODO:
-    * Calculate a Jacobian here.
-  */
+    // Initializing jacobian
+    MatrixXd Hj(3,4);
+    
+	// Getting state parameters
+	float px = x_state(0);
+	float py = x_state(1);
+	float vx = x_state(2);
+	float vy = x_state(3);
+
+	// Computing terms
+    float d1 = px * px + py * py;
+	float d2 = sqrt(d1);
+	float d3 = (d1 * d2);
+	
+    // Checking denominator for division
+    if (d1 < 0.0001) {
+	    cout << "Error in CalculateJacobian() - No division by zero." << endl;
+	}
+    
+    else {
+	// Computing Jacobian
+        Hj <<   px / d2, py / d2, 0, 0,
+                -py / d1, px / d1, 0, 0,
+                py * (vx * py - vy * px) / d3, px * (vy * px - vx * py) / d3, px / d2, py / d2;
+	}
+	return Hj;
 }
